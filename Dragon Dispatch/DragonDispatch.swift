193cc4cf4d9b8ea131b3b193017b63cc1040fbad
George Green <george@thegreenfamily.net>
2014-09-26 22:59:13 +0100

    Added initial version of the DRDispatchQueue class and some global helper functions for dispatching blocks of code.

---
+++ Dragon Dispatch/DragonDispatch.swift
@@ -0,0 +1,71 @@
+//
+//  DragonDispatch.swift
+//  Dragon Dispatch
+//
+//  Created by George Green on 26/09/2014.
+//  Copyright (c) 2014 The Swift Guru. All rights reserved.
+//
+
+import Foundation
+
+typealias DRDispatchBlock = (() -> Void)
+
+/// The priority of a global concurrent queue.
+enum DRQueuePriority {
+	case Low
+	case Default
+	case High
+	case Background
+	
+	func toConst() -> dispatch_queue_priority_t {
+		switch self {
+		case .Low:
+			return DISPATCH_QUEUE_PRIORITY_LOW
+		case .High:
+			return DISPATCH_QUEUE_PRIORITY_HIGH
+		case .Background:
+			return DISPATCH_QUEUE_PRIORITY_BACKGROUND
+		default:
+			return DISPATCH_QUEUE_PRIORITY_DEFAULT
+		}
+	}
+}
+
+/// The type of a dispatch queue.
+enum DRQueueType {
+	/// A serial queue will execute all blocks dispatch to it one after another.
+	/// It will wait for the first block to complete and then execute the second one after it and so on...
+	/// Guarantees that all blocks dispatched will be executed in the order that they are dispatched.
+	case Serial
+	/// A concurrent will potentially run multiple blocks at the same time. 
+	/// Blocks dispatched to a concurrent queue are guaranteed to be started in the order that they are dispatched,
+	/// but they will not necessarily finish in the same order.
+	case Concurrent
+	
+	func toConst() -> dispatch_queue_attr_t {
+		switch self {
+			case .Serial:
+				return DISPATCH_QUEUE_SERIAL
+			case .Concurrent:
+				return DISPATCH_QUEUE_CONCURRENT
+		}
+	}
+}
+
+/// Dispatch a block of code for execution on the specifed priority global concurrent queue.
+/// Returns immediately and the block will be executed at some point in the future.
+/// @returns A reference to the queue that the block will be executed on.
+func DRDispatchAsync(block : DRDispatchBlock, priority: DRQueuePriority = .Default) -> DRDispatchQueue {
+	let queue = DRDispatchQueue.globalQueueWithPriority(priority)
+	queue.dispatchAsync(block)
+	return queue
+}
+
+/// Dispatch a block of code for execution on the specified priority global concurrent queue.
+/// Returns once execution of the block has completed.
+/// @returns A reference to the queue that the block of code was executed on.
+func DRDispatchSync(block: DRDispatchBlock, priority: DRQueuePriority = .Default) -> DRDispatchQueue {
+	let queue = DRDispatchQueue.globalQueueWithPriority(priority)
+	queue.dispatchSync(block)
+	return queue
+}
---
+++ Dragon Dispatch/DragonDispatchQueue.swift
@@ -0,0 +1,92 @@
+//
+//  DragonDispatchQueue.swift
+//  Dragon Dispatch
+//
+//  Created by George Green on 26/09/2014.
+//  Copyright (c) 2014 The Swift Guru. All rights reserved.
+//
+
+import Foundation
+
+/// Compare two DRDispatchQueue objects. Two are equal if they represent the same underlying dispatch_queue_t.
+func == (left: DRDispatchQueue, right: DRDispatchQueue) -> Bool {
+	return left._queue.isEqual(right._queue)
+}
+
+private let _mainQueue = DRDispatchQueue(queue: dispatch_get_main_queue())
+private let _lowPriorityQueue = DRDispatchQueue.globalQueueWithPriority(.Low)
+private let _defaultPriorityQueue = DRDispatchQueue.globalQueueWithPriority(.Default)
+private let _highPriorityQueue = DRDispatchQueue.globalQueueWithPriority(.High)
+private let _backgroundPriorityQueue = DRDispatchQueue.globalQueueWithPriority(.Background)
+
+/// DRDispatchQueue
+/// This class represents a queue on which blocks of code may be dispatched.
+class DRDispatchQueue {
+	
+	// MARK: - Private Variables
+	
+	/// The underlying dispatch_queue_t object that is represented by this object
+	private let _queue: dispatch_queue_t
+	
+	// MARK: - Public Variables
+	
+	
+	/// The priority of the queue if this object represent a global queue. Otherwise nil.
+	let priority: DRQueuePriority?
+	/// The type of the queue. Either .Serial or .Concurrent.
+	let type: DRQueueType?
+	
+	// MARK: - Class Methods
+	
+	class func mainQueue() -> DRDispatchQueue {
+		return _mainQueue
+	}
+	
+	class func globalQueueWithPriority(priority: DRQueuePriority) -> DRDispatchQueue {
+		switch priority {
+		case .Low:
+			return _lowPriorityQueue
+		case .High:
+			return _highPriorityQueue
+		case .Background:
+			return _backgroundPriorityQueue
+		case .Default:
+			return _defaultPriorityQueue
+		}
+	}
+	
+	// MARK: - Convienence Initialisers
+	
+	
+	// MARK: - Object Lifecycle Methods
+	
+	/// Create a queue object that represents a dispatch queue with the specitied type.
+	init(type: DRQueueType) {
+		_queue = dispatch_queue_create("Dragon Dispatch Queue", type.toConst())
+		self.type = type
+	}
+	
+	/// Create a queue object that represents the specified dispatch queue.
+	init(queue: dispatch_queue_t) {
+		_queue = queue
+	}
+	
+	// MARK: - External Action Methods
+	
+	/// A convenience method for dispatchAsync.
+	func dispatch(block: DRDispatchBlock) {
+		dispatchAsync(block)
+	}
+	
+	/// Executes the passed in block on this queue. Will not return until the block has been executed.
+	func dispatchSync(block: DRDispatchBlock) {
+		dispatch_sync(_queue, block)
+	}
+	
+	/// Executes the passed in block on this queue. Will return immediatly, and the block will be executed
+	/// at some point in the future.
+	func dispatchAsync(block: DRDispatchBlock) {
+		dispatch_async(_queue, block)
+	}
+	
+}
